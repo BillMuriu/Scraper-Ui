cacf6e5e82b33c5d124d549c514948d234c44058
unknown <njorogekamau63@gmail.com>
2025-02-10 15:44:29 +0300

    I implemented darkmode and the 404 page

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -12,10 +12,22 @@ import {
 
 function BreadcrumbHeader() {
   const pathName = usePathname();
-  const path = pathName === "/" ? [""] : pathName?.split("/");
+  const paths = pathName === "/" ? [""] : pathName?.split("/");
   return (
     <div className="flex items-center flex-start">
-      <Breadcrumb></Breadcrumb>
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, index) => (
+            <React.Fragment key={index}>
+              <BreadcrumbItem>
+                <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                  {path === "" ? "home" : path}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
     </div>
   );
 }
