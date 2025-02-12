da81f0b486fd3b70531312c38f5920cbc4d9ce27
unknown <njorogekamau63@gmail.com>
2025-02-12 16:05:18 +0300

    It's time to implement zod

---
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Layers2Icon } from "lucide-react";
+import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { useForm } from "react-hook-form";
+import { createWorkflowSchema } from "@/schema/workflow";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
+  const [open, setOpen] = useState(false);
+  const form = useForm<z.infer<typeof createWorkflowSchema>>({
+    resolver: zodResolver(createWorkflowSchema),
+    defaultValues: {},
+  });
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>{triggerText ?? "Create workflow"}</Button>
+      </DialogTrigger>
+      <DialogContent className="px-0">
+        <CustomDialogHeader
+          icon={Layers2Icon}
+          title="Create Workflow"
+          subTitle="Start building your workflow"
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateWorkflowDialog;
