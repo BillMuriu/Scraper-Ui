12a2392c45d502546895c45e757711d598caa292
unknown <njorogekamau63@gmail.com>
2025-02-12 15:04:45 +0300

    Install and setup react-query

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -1,6 +1,9 @@
+import { GetWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
 import { Skeleton } from "@/components/ui/skeleton";
 import { waitFor } from "@/lib/helper/waitFor";
 import React, { Suspense } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle, InboxIcon } from "lucide-react";
 
 function page() {
   return (
@@ -31,7 +34,36 @@ function UserWorkflowsSkeleton() {
 }
 
 async function UserWorkflows() {
-  await waitFor(3000);
+  //   await waitFor(3000);
+  const workflows = await GetWorkflowsForUser();
+  //   const workflows = null;
+  if (!workflows) {
+    return (
+      <Alert variant={"destructive"}>
+        <AlertCircle className="w-4 h-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Something went wrong. Please try again later
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (workflows.length === 0) {
+    return (
+      <div className="flex flex-col gap-4 h-full items-center justify-center">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <InboxIcon size={40} className="stroke-primary" />
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">No workflow created yet</p>
+          <p className="text-sm text-muted-foreground">
+            Click the button below to create your first workflow
+          </p>
+        </div>
+      </div>
+    );
+  }
   return <div></div>;
 }
 
