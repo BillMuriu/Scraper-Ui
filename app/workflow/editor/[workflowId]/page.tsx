341d591caaaddf8cd9363558e418eb169de7a8dd
unknown <njorogekamau63@gmail.com>
2025-02-13 12:48:32 +0300

    instantiating react-flow

---
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -0,0 +1,28 @@
+import { waitFor } from "@/lib/helper/waitFor";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+import Editor from "../../_components/Editor";
+
+async function page({ params }: { params: { workflowId: string } }) {
+  const { workflowId } = params;
+  const { userId } = auth();
+  if (!userId) return <div>unauthenticated</div>;
+
+  // await waitFor(1000);
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id: workflowId,
+      userId,
+    },
+  });
+
+  if (!workflow) {
+    return <div> Workflow not found</div>;
+  }
+
+  // return <pre>{JSON.stringify(workflow, null, 4)}</pre>;
+  return <Editor workflow={workflow} />;
+}
+
+export default page;
