c889be931fd758de3cde54990ed1bd29628a6ce1
unknown <njorogekamau63@gmail.com>
2025-02-16 01:20:30 +0300

    I created the extract task from element task and recorded it in the registry

---
+++ lib/workflow/task/ExtractTextFromElement.tsx
@@ -0,0 +1,34 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { LucideProps, TextIcon } from "lucide-react";
+
+export const ExtractTextFromElementTask = {
+  type: TaskType.EXTRACT_TEXT_FROM_ELEMENT,
+  label: "Get text from HTML",
+  icon: (props: LucideProps) => (
+    <TextIcon className="stroke-pink-400" {...props} />
+  ),
+  isEntryPoint: false,
+  inputs: [
+    {
+      name: "Html",
+      type: TaskParamType.STRING,
+      required: true,
+    },
+    {
+      name: "Selector",
+      type: TaskParamType.STRING,
+      required: true,
+    },
+  ],
+
+  outputs: [
+    {
+      name: "Extracted Text",
+      type: TaskParamType.STRING,
+    },
+    {
+      name: "Web Page",
+      type: TaskParamType.BROWSER_INSTANCE,
+    },
+  ],
+};
