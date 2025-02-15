bc9b605156f50b74ff68cd0838e05cb50b5f1172
unknown <njorogekamau63@gmail.com>
2025-02-15 18:32:08 +0300

    I created v1 of the aside menu for the editor

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "./FlowEditor";
 import Topbar from "./topbar/Topbar";
+import TaskMenu from "./TaskMenu";
 
 function Editor({ workflow }: { workflow: Workflow }) {
   //   return <div>Editor</div>;
@@ -13,6 +14,7 @@ function Editor({ workflow }: { workflow: Workflow }) {
       <div className="flex flex-col h-full w-full overflow-hidden">
         <Topbar title="Workflow Page" subtitle="" workflowId={workflow.id} />
         <section className="flex h-full overflow-auto">
+          <TaskMenu />
           <FlowEditor workflow={workflow} />
         </section>
       </div>
