732b13da3720474b4ea2a886576c618c0cf41eee
unknown <njorogekamau63@gmail.com>
2025-02-17 15:55:54 +0300

    I created the runworkflow server action

---
+++ actions/workflows/runWorkflow.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
+import { WorkflowExecutionPlan } from "@/types/workflow";
+import { auth } from "@clerk/nextjs/server";
+
+export async function RunWorkflow(form: {
+  workflowId: string;
+  flowDefinition?: string;
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("unauthenticated");
+  }
+
+  const { workflowId, flowDefinition } = form;
+
+  if (!workflowId) {
+    throw new Error("Workflow id required");
+  }
+
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      userId,
+      id: workflowId,
+    },
+  });
+
+  if (!workflow) {
+    throw new Error("workflow not found");
+  }
+
+  let executionPlan: WorkflowExecutionPlan;
+  if (!flowDefinition) {
+    throw new Error("Flow definition is not defined");
+  }
+
+  const flow = JSON.parse(flowDefinition);
+
+  const result = FlowToExecutionPlan(flow.nodes, flow.edges);
+
+  if (result.error) {
+    throw new Error("flow definition not valid");
+  }
+
+  if (!result.executionPlan) {
+    throw new Error("no execution plan generated");
+  }
+
+  executionPlan = result.executionPlan;
+  console.log("Execution plan", executionPlan);
+}
